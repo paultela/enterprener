92f5b0817820437a6eabf441b986a5bfd348c228
Paul Tela <paul@paultela.com>
2013-03-02 02:04:53 -0500

    Initial project checkin

---
+++ replaceWords.js
@@ -0,0 +1,34 @@
+(function($) {
+
+	function buildRegex() {
+		var spellings = ['(E|e)nt', ['rep', 'ep'], ['ren', 'en', 'ern'], ['eur', 'er']];
+		//var endings = ['', 'ship', 'ial'];
+		var parts = [];
+
+		for (var i = 0; i < spellings.length; i++) {
+			if (spellings[i] instanceof Array) {
+				parts.push('(?:');
+					for (var j = 0; j < spellings[i].length; j++) {
+						parts.push(spellings[i][j]);
+						parts.push('|');
+					}
+					parts.pop();
+					parts.push(')');
+			} else { // String
+				parts.push(spellings[i]);
+			}
+		}
+
+		return new RegExp(parts.join(''), 'g');
+	}
+
+var regex = buildRegex();
+
+function changeStuff(e) { 
+	$(this).val($(this).val().replace(regex, "$1ntrepreneur"));
+}
+
+$('input').on('change', changeStuff);
+$('textarea').on('change', changeStuff);
+
+})(jQuery);
